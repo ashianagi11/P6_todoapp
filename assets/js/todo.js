5cc779921f837d117533a1efb9891541c58ff1e4
Ashia Nagi <ashianagi00@gmail.com>
2019-07-26 10:23:43 -0400

    create to do app

---
+++ assets/js/todo.js
@@ -0,0 +1,19 @@
+//checking off any to do: 
+$("input[type='text']").keypress(function(event){ 
+    if(event.which === 13) { //13 is code for enter key. (KeyBoardEvent Value)
+        var todoText = $(this).val(); 
+        $(this).val(""); //replaces value with a empty string. (functionwise it removes the text after enter.)
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText +  "</li>");
+    }
+})
+
+$("ul").on("click", "li", function() { 
+    $(this).toggleClass("completed"); 
+})
+
+$("ul").on("click", "span", function(e) {
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove(); 
+    });
+    e.stopPropagation(); //stops the event from bubbling ups
+})
